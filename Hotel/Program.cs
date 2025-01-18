54564f20fb4277c2b1fffdb1d68a20625d5446bb
Михаил <134938010+JatyPruducts@users.noreply.github.com>
2025-01-18 21:23:50 +0300

    Update appsettings.json and appsettings.Development.json. Make dependency injection in  Program.cs

--- Hotel/Program.cs
+++ Hotel/Program.cs
@@ -1,9 +1,26 @@
+using Hotel.Domain.Interfaces;
+using Hotel.Domain.Services;
+using Hotel.Infrastructure.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Подключаем контроллеры
+builder.Services.AddControllers();
+
+// Регистрируем Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Читаем строку подключения из appsettings.json
+var configuration = builder.Configuration;
+string connectionString = configuration.GetConnectionString("DefaultConnection")
+                          ?? throw new Exception("Connection string 'DefaultConnection' not found in appsettings.json");
+
+// Регистрируем репозитории  (_ - лямбда выражение
+builder.Services.AddScoped<IDepartureRepository>(_ => new DepartureRepository(connectionString));
+
+// Регистрируем сервисы
+builder.Services.AddScoped<IDepartureService, DepartureService>();
 
 var app = builder.Build();
 
@@ -16,29 +33,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
-
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
